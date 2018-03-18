ef9521957dc200b6dfb00c641fcb83130f36cdb6
lizengjie <18661619660@163.com>
2018-03-18 10:23:37 +0800

    背景虚化

--- app/src/main/java/com/madeinlk/firstmenu/AboutusActivity.java
+++ app/src/main/java/com/madeinlk/firstmenu/AboutusActivity.java
@@ -2,14 +2,17 @@ package com.madeinlk.firstmenu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.madeinlk.R;
 
 public class AboutusActivity extends Activity {
-
+    private View mback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutus);
+        mback = findViewById(R.id.aboutus_main);
+        mback.getBackground().setAlpha(100);
     }
 }
