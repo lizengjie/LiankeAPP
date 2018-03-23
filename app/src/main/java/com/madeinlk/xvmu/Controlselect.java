bcaa36f7bb31aa2f8726535abd618497795773cb
lizengjie <18661619660@163.com>
2018-03-23 12:46:23 +0800

    增加id配置

--- app/src/main/java/com/madeinlk/xvmu/Controlselect.java
+++ app/src/main/java/com/madeinlk/xvmu/Controlselect.java
@@ -5,47 +5,104 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.madeinlk.R;
+
+import java.util.ArrayList;
 
 public class Controlselect extends Activity {
     private Button mBtn;
     private RadioButton mRadioButton;
     private RadioGroup mRadioGroup1;
+    private ImageView mImageView;
+    private Spinner mSpinner1,mSpinner2;
+    private TextView mTextView;
     public String instr=null;
-    public int en=-1;
+    private int num_sel1,num_sel2;
+    public int sel_id;
+    public int cate=-1;
+    int[] num={0,1,2,3,4,5,6,7,8,9};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controlselect);
         //radiobutton事件
+        mImageView = (ImageView) findViewById(R.id.control_select_IM1);
         mRadioGroup1 = (RadioGroup) findViewById(R.id.rg1);
         mRadioGroup1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-//                mRadioButton = (RadioButton) group.findViewById(checkedId);
-                en =checkedId;
+
                 switch(checkedId){
                     case R.id.Radio1:
                         instr = "饲喂器";
+                        cate=0;
+                        mImageView.setImageResource(R.drawable.siweiqi);
                         break;
                     case R.id.Radio2:
                         instr = "饲喂站";
+                        cate=1;
+                        mImageView.setImageResource(R.drawable.siweizhan);
                         break;
                     case R.id.Radio3:
                         instr = "环控";
+                        cate=2;
+                        mImageView.setImageResource(R.drawable.huankong);
                         break;
                     case R.id.Radio4:
                         instr = "料线";
+                        cate=3;
+                        mImageView.setImageResource(R.drawable.liaoxian);
                         break;
                     default:
                         instr = "请选择设备";
-                        en=-1;
+                        cate=-1;
                 }
+            }
+        });
+        mSpinner1= (Spinner) findViewById(R.id.spinner1);
+        mSpinner2= (Spinner) findViewById(R.id.spinner2);
+        final ArrayList arrayList=new ArrayList();
+        for(int i=0;i<num.length;i++)
+        {
+            arrayList.add(num[i]);
+        }
+        final ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner1.setAdapter(arrayAdapter);
+        mSpinner2.setAdapter(arrayAdapter);
+        mSpinner1.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                num_sel1 = (int) mSpinner1.getSelectedItem();
+                mTextView= (TextView) findViewById(R.id.dis_id);
+                sel_id=num_sel1*10+num_sel2;
+                mTextView.setText("设备ID是" + sel_id);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        mSpinner2.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                num_sel2 = (int) mSpinner2.getSelectedItem();
+                mTextView= (TextView) findViewById(R.id.dis_id);
+                sel_id=num_sel1*10+num_sel2;
+                mTextView.setText("设备ID是" + sel_id);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
 
@@ -56,14 +113,18 @@ public class Controlselect extends Activity {
             @Override
             public void onClick(View view) {
                 Bundle bundle = new Bundle();
-                bundle.putString("instr",instr);
+                bundle.putString("cata",instr);
+                bundle.putInt("id",sel_id);
                 Intent intent = new Intent(Controlselect.this,view_control.class);
                 intent.putExtras(bundle);
-                if(en !=-1){
-                    startActivity(intent);
-                }
-                else{
+                if(cate<0 ||cate>3){
                     Toast.makeText(Controlselect.this,"请选择设备",Toast.LENGTH_SHORT).show();
+                }
+                else if(sel_id==0){
+                    Toast.makeText(Controlselect.this,"请选择ID",Toast.LENGTH_SHORT).show();
+                }
+                else if((cate>=0&&cate<=3)&&sel_id!=0){
+                    startActivity(intent);
                 }
 
             }
--- app/src/main/java/com/madeinlk/xvmu/view_control.java
+++ app/src/main/java/com/madeinlk/xvmu/view_control.java
@@ -7,14 +7,18 @@ import android.widget.TextView;
 import com.madeinlk.R;
 
 public class view_control extends Activity {
-    private TextView mTextView1;
+    private TextView mTextView1,mTextView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_control);
         mTextView1 = (TextView) findViewById(R.id.input_tv1);
+        mTextView2 = (TextView) findViewById(R.id.input_tv2);
         Bundle bundle = getIntent().getExtras();
-        String string=bundle.getString("instr");
+        String string=bundle.getString("cata");
+        int id = bundle.getInt("id");
+        String string2= String.valueOf(id);
         mTextView1.setText(string);
+        mTextView2.setText(string2);
     }
 }
